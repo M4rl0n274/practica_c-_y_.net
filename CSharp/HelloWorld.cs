4eba471ffa1380dfb09fb0912707df09b42874a9
M4rl0n274 <marlonquinteri274@gmail.com>
2025-07-15 17:36:40 -0500

    tipos de datos

--- CSharp/HelloWorld.cs
+++ CSharp/HelloWorld.cs
@@ -1,2 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C#");
+﻿using System;
+
+namespace CSharpHelloWorld
+{
+
+    class HelloWorld
+    {
+
+        static void Main(string[] args)
+        {
+            // Hola mundo
+            Console.WriteLine("Hello, C#");
+
+            /*
+            Esto 
+            es 
+            un
+            comentario
+            */
+
+            string myString = "Esto es una cadena de texto";
+            myString = "Aqui cambio el valor de la cadena de texto";
+            Console.WriteLine(myString);
+
+            int myInt = 10;
+            myInt = myInt + 4;
+            Console.WriteLine(myInt);
+            Console.WriteLine(myInt - 1);
+            Console.WriteLine(myInt);
+
+            double myDouble = 6.5f;
+            Console.WriteLine(myDouble);
+
+            float myFloat = 6.5f;
+            Console.WriteLine(myFloat);
+
+            Console.WriteLine(myInt + myFloat + myDouble);
+
+            bool myBool = true;
+            Console.WriteLine(myBool);
+
+
+            dynamic myDinamic = 6;
+            myDinamic = "Mi dato dinamico";
+            Console.WriteLine(myDinamic);
+
+            var myVar = "Mi variable de tipo inferido";
+            // myVar = 6; Error 
+            Console.WriteLine(myVar);
+
+            Console.WriteLine($"El valor de mi entero es {myInt} y el de mi bool {myBool} ");
+
+
+            const string MyConst = "mi constante";
+            Console.WriteLine(MyConst);
+
+            //
+
+
+
+        }
+    }
+
+}
--- CSharp/obj/Debug/net9.0/CSharp.AssemblyInfo.cs
+++ CSharp/obj/Debug/net9.0/CSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba4fdda1af3b3c849cbc4255b17c19420c14aa04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+957f7a72d8e913424189e34d7548cc72e5f5a839")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
