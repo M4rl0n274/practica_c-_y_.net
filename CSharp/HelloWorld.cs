8ee0d4d116e075a6e580a1c4d2ea4e3352c395ff
M4rl0n274 <marlonquinteri274@gmail.com>
2025-07-15 17:59:24 -0500

    estructuras bucles

--- CSharp/HelloWorld.cs
+++ CSharp/HelloWorld.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
 namespace CSharpHelloWorld
 {
@@ -22,7 +23,7 @@ namespace CSharpHelloWorld
             myString = "Aqui cambio el valor de la cadena de texto";
             Console.WriteLine(myString);
 
-            int myInt = 10;
+            int myInt = 11;
             myInt = myInt + 4;
             Console.WriteLine(myInt);
             Console.WriteLine(myInt - 1);
@@ -54,11 +55,84 @@ namespace CSharpHelloWorld
             const string MyConst = "mi constante";
             Console.WriteLine(MyConst);
 
-            //
+            // Estructuras
+
+            var myArray = new string[] { "Marlon", "Quintero" };
+            Console.WriteLine(myArray[0]);
+            Console.WriteLine(myArray[1]);
+
+            var myDictionary = new Dictionary<string, int>
+            {
+                {"Marlon", 20},
+                {"Marlon 2", 21},
+                {"Marlon 3", 22}
+            };
+            Console.WriteLine(myDictionary["Marlon"]);
+
+            var mySet = new HashSet<string>{
+                 "Marlon", "Quintero"};
+            Console.WriteLine(mySet);
+
+            var myTuple = ("Marlon", "Quintero");
+            Console.WriteLine(myTuple);
+
+            // Bucles
+
+            for (int index = 0; index < 10; index++)
+            {
+                Console.WriteLine(index);
+            }
+
+            foreach (var myItem in myArray)
+            {
+                Console.WriteLine(myItem);
+            }
+
+            foreach (var myItem in myDictionary)
+            {
+                Console.WriteLine(myItem);
+            }
+
+            foreach (var myItem in mySet)
+            {
+                Console.WriteLine(myItem);
+            }
+
+            //Flujos
+
+            myInt = 11;
+
+            if (myInt == 11 && myBool == true)
+            {
+                Console.WriteLine("El valor es 11");
+            }
+            else if (myInt == 12 || myBool == false)
+            {
+                Console.WriteLine("El valor es 12");
+
+            }
+            else
+            {
+                Console.WriteLine("El valor no es 11 ni 12");
+
+            }
 
 
+            MyFunction();
+            Console.WriteLine(MyFunctionWithReturn(5));
 
         }
+
+        static void MyFunction()
+        {
+            Console.WriteLine("Mi funcion");
+        }
+        static int MyFunctionWithReturn(int param)
+        {
+            return 10 + param;
+        }
+
+
     }
 
 }
